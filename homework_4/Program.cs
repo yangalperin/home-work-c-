b7f52b53be3f9157a839815107d381af19411692
Ян Гальперин <yan@MacBook-Air-An.local>
2022-08-08 09:34:24 +0300

    HW_4

---
+++ homework_4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+int count = 2;
+while(count <= number)
+{
+    Console.Write(count + " ");
+    count = count + 2;
+}
+
